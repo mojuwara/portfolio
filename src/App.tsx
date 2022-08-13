cb77e30f07997b46f0ac3f2ba09eabf5e4a359ca
Mahamadou Juwara <mj1803@nyu.edu>
2022-08-13 16:33:53 -0400

    Rough design. MVP

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Tags } from './ProjectTags';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<Contact />
+			<About />
+			<Projects />
     </div>
   );
+}
+
+// TODO: Host resume on AWS
+const Contact = () => {
+	return (
+		<div className="contact">
+			<a href="http://www.google.com" target="_blank" rel="noreferrer">
+				<Button variant="outlined">Resume</Button>
+			</a>
+
+			<a href="https://github.com/mojuwara" target="_blank" rel="noreferrer">
+				<img src="/github-icon.png" alt="Github Icon" width={30} />
+			</a>
+
+			<a href='https://www.linkedin.com/in/mahamadou-juwara/' target="_blank" rel="noreferrer">
+				<img src="/linkedin-icon.png" alt="LinkedIn Icon" width={30} />
+			</a>
+		</div>
+	)
+}
+
+const About = () => {
+	return (
+		<div className="about-section">
+			<div>
+				<h1>Mahamadou Juwara</h1>
+				<h4>Software Engineer II, Bank of America</h4>
+			</div>
+			{/* <div>
+				<h3>I am a software engineer at Bank of America with passion for distributed systems, backend development and programming in general.</h3>
+				<h3>Please have a look at some of my personal projects.</h3>
+			</div> */}
+		</div>
+	)
+}
+
+const Projects = () => {
+	const projectList = [
+		{
+			href: "https://raft.mojuwara.com",
+			title: "Raft Consensus Algorithm Demo",
+			tags: ["AWS", "AWS Lambda", "Docker", "GO", "https", "Linux", "React", "RPC", "Multithreading", "Signals", "TypeScript", "WebSocket"],
+		}
+	]
+	return (
+		<div className='projects-container'>
+			<div className='projects'>
+				<h2>Projects</h2>
+				{projectList.map((p, ndx) => <Project key={ndx} {...p}/>)}
+			</div>
+		</div>
+	)
+}
+
+type ProjectProps = {
+	href: string;
+	title: string;
+	tags: string[];
+	// display: string;
+}
+const Project = ({href, title, tags}: ProjectProps) => {
+	const tagStyle = {
+		margin: 0.2,
+		padding: 0.6,
+		borderRadius: 3,
+		textAlign: "center",
+		display: "inline-block",
+	}
+
+	return (
+		<div className="project">
+			<a href={href} target="_blank" rel="noreferrer">
+				<h3>{title}</h3>
+			</a>
+			<img src="/placeholder-image.png" alt={title} width="70%" />
+			<div className="tag-container">
+				{tags.map((t, ndx) => <Paper key={ndx} elevation={1} sx={{ ...tagStyle, backgroundColor: Tags[t] }}>{t}</Paper>)}
+			</div>
+		</div>
+	)
 }
 
 export default App;
---
+++ src/ProjectTags.tsx
@@ -0,0 +1,20 @@
+// Alphabetical order
+interface TagObject {
+	[index: string]: string;
+}
+
+export const Tags:TagObject = {
+	"AWS"							: "#ec7211",
+	"AWS Lambda"			: "#ec7211",
+	"Docker"					: "#0073ec",
+	"GO"							: "#007d9c",
+	"https"						: "#1fa22c",
+	"Linux"						: "#0c6d1a",	// Or #e95420
+	"React"						: "#61dafb",
+	// "RESTful"			: "",
+	"RPC"							: "#ab68c8",
+	"Multithreading"	:	"#ab68c8",
+	"Signals"					: "#ec331d",
+	"TypeScript"			: "#3178c6",
+	"WebSocket"				: "#c741d5",
+}
